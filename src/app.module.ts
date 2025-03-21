b3803164c83c3484766822c1d5d89e1501e6436c
Moise DEKI [OBA CI] <mdeki@orangebank.ci>
2025-03-21 07:16:30 +0000

    Connectivité à la BD

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProduitModule } from './config/produit/produit.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'gstock_db',
+      entities: [],
+      synchronize: true,
+    }),
     ConfigModule.forRoot({
       isGlobal: true
     }),
-     ProduitModule],
+    ProduitModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
