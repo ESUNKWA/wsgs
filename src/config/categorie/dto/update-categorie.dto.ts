fbca2cc78349ff64bdff945baf3b1d2229696247
Moise DEKI [OBA CI] <mdeki@orangebank.ci>
2025-03-21 22:21:51 +0000

    dto categorie

--- src/config/categorie/dto/update-categorie.dto.ts
+++ src/config/categorie/dto/update-categorie.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCategorieDto } from './create-categorie.dto';
+import { IsNotEmpty } from 'class-validator';
 
-export class UpdateCategorieDto extends PartialType(CreateCategorieDto) {}
+export class UpdateCategorieDto extends PartialType(CreateCategorieDto) {
+    @IsNotEmpty({
+        message: 'Veuillez saisir le nom de la catégorie'
+    })
+    nom: string;
+    description: string;
+}
