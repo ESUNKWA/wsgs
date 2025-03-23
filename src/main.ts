3323ba46bdf4d9b60b864b9432428cdff806a300
Moise DEKI [OBA CI] <mdeki@orangebank.ci>
2025-03-23 13:16:08 +0000

    redefinition do port

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, documentFactory, {jsonDocumentUrl: 'swagger/json',});
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
   //const confiService = app.get(ConfigService);
   //await app.listen(confiService.get('APP_PORT'));
 }
